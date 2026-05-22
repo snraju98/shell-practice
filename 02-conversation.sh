fc9453363135ff66b0075fbd1dca9882dae3305c
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-22 15:09:22 +0530

    conversations

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Trump: hey iran,we are going to blast you"
-echo "Iran: yes, please you are welcome"
-echo "Trump: hey iran iam serious"
+PERSON1=trump
+PERSON2=iran
 
+echo "$PERSON1: hey iran,we are going to blast you"
+echo "$PERSON2: yes, please you are welcome"
+echo "$PERSON1: hey $PERSON2 iam serious"
+echo "$PERSON2: $PERSON1, we are serious
