e69500111422632b14281fa113ef8084fe75f7fa
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-22 15:53:19 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: hey iran,we are going to blast you"
+echo "$PERSON2: yes, please you are welcome"
+echo "$PERSON1: hey $PERSON2 iam serious"
+echo "$PERSON2: $PERSON1, we are serious"
