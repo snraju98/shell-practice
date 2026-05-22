f812356c460f378533265767ec6daccfb00919f6
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-22 15:19:54 +0530

    added conversation script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -6,4 +6,4 @@ PERSON2=ukraine
 echo "$PERSON1: hey iran,we are going to blast you"
 echo "$PERSON2: yes, please you are welcome"
 echo "$PERSON1: hey $PERSON2 iam serious"
-echo "$PERSON2: $PERSON1, we are serious
+echo "$PERSON2: $PERSON1, we are serious"
