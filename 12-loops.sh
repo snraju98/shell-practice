cee1590a93132b6e2c9b1be9d12e58d5f0491c47
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-25 16:54:01 +0530

    api doc added

--- 12-loops.sh
+++ 12-loops.sh
@@ -5,6 +5,7 @@
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 #check root acces or not
 if [ $USERID -ne 0 ]; then
@@ -15,17 +16,25 @@ fi
 #first arg  > what are you trying to install
 #second arg > exit code
 VALIDATE(){
-     if [ $2 -ne 0 ]; then
-      echo "installing mysql is ....failed"
-      exit 1
-   else
-      echo "installing my sql is ...succes"
-   fi
+    if [ $2 -ne 0 ]; then 
+       echo $TIMESTAMP [ERROR] "installing $1 is ....failed" | tee -a $LOGS_FILE
+       exit 1
+    else
+       echo $TIMESTAMP [INFO] "installing $1 is ...succes"   | tee -a $LOGS_FILE
+    fi
 }
 
 
 for package in $@
 do
    echo "Installing $pacakage"
+   dnf list installed $pacakage  &>> $LOGS_FILE
+   if [ $? -ne 0 ]; then
+      dnf install $pacakage -y &>> $LOGS_FILE
+      VALIDATE "Installing $pacakage" $?
+    else
+     echo "$pacakage already installed... skipping"
+   fi
 done
+
 
