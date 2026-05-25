82b2a8472cc1f48204e0b66e3e012b129c8d135b
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-25 17:30:45 +0530

    api doc added

--- 15-set.sh
+++ 15-set.sh
@@ -11,6 +11,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+trap 'echo "error at $LINENO", command: $BASH_COMMAND" ' ERR
+
 #check root acces or not
 if [ $USERID -ne 0 ]; then
    echo "please run this script with root access"
