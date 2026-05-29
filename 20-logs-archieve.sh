0cf96200c6ed21dcbb040efb7e0fb0aca9cc7da7
snraju98 <nagarajusagiraju123@gmail.com>
2026-05-29 12:55:05 +0530

    api added

--- 20-logs-archieve.sh
+++ 20-logs-archieve.sh
@@ -4,19 +4,21 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}
 
-if [ -z "$SOURCE_DIR" ] | [ -Z "$DEST_DIR" ]; then
-   echo "Either source directory or destinaton directory empty"
-   echo "USAGE:: $0 [source_dir] [dest_dir] [days:default 14]"
-   exit 1
+if [ -z "$SOURCE_DIR" ] || [ -z "$DEST_DIR" ]; then
+    echo "Either source directory or destination directory empty"
+    echo "USAGE:: $0 [source_dir] [dest_dir] [days:default 14]"
+    exit 1
 fi
 
 if [ ! -d "$SOURCE_DIR" ]; then
-   echo "Source directory: $SOURCE_DIR does not exists"
-   exit 1
+    echo "Source directory: $SOURCE_DIR does not exist"
+    exit 1
 fi
 
 if [ ! -d "$DEST_DIR" ]; then
-   echo "Destination directory: $DEST_DIR does not exists"
-   exit 1
+    echo "Destination directory: $DEST_DIR does not exist"
+    exit 1
 fi
+
+
 
